5f75198730759c6a14bb1879438f2108a33e5dbc
hyubuki <suhyuk.ts@gmail.com>
2025-08-01 09:34:12 +0900

    chore(flyway): add Flyway dependencies and initial schema scripts

---
+++ service/article/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,12 @@
+create database if not exists article;
+
+create table if not exists article.article
+(
+    article_id  bigint        not null primary key,
+    title       varchar(100)  not null,
+    content     varchar(3000) not null,
+    board_id    bigint        not null,
+    writer_id   bigint        not null,
+    created_at  datetime      not null,
+    modified_at datetime      not null
+)
